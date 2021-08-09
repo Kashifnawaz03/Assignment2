077b29db613afb386ba50382501bf1dd031c038f
Kashifnawaz03 <72625629+Kashifnawaz03@users.noreply.github.com>
2021-08-09 16:50:38 +0500

    Update Assignment2PIAIC80749.sol

--- Assignment2PIAIC80749.sol
+++ Assignment2PIAIC80749.sol
@@ -1,3 +1,16 @@
+/**Create Crypto Bank Contract
+
+    1) The owner can start the bank with initial deposit/capital in ether (min 50 eths)
+    2) Only the owner can close the bank. Upon closing the balance should return to the Owner
+    3) Anyone can open an account in the bank for Account opening they need to deposit ether with address
+    4) Bank will maintain balances of accounts
+    5) Anyone can deposit in the bank
+    6) Only valid account holders can withdraw
+    7) First 5 accounts will get a bonus of 1 ether in bonus
+    8) Account holder can inquiry balance
+The depositor can request for closing an account
+*/
+
 pragma solidity ^0.8.0;
 
 contract CryptoBank{
